b4345110b9d1b251c42484b997046c6908ed9e55
Oscar Barajas Tavares <oscar@arepa.dev>
2020-04-02 12:24:33 -0500

    Here be Dragons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,25 @@
+<script context="module">
+  export async function preload() {
+    let data = {};
+    const response = await this.fetch(
+      `https://us-central1-pugstagram-co.cloudfunctions.net/data`
+    );
+    data = await response.json();
+    return { data };
+  }
+</script>
+
 <script>
-  import { onMount } from "svelte";
   import Header from "../components/Header.svelte";
   import Main from "../components/Main.svelte";
   import Sidebar from "../components/Sidebar.svelte";
   import TimeLine from "../components/TimeLine.svelte";
-
-  let data = {};
-  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
-
-  onMount(async () => {
-    const response = await fetch(API);
-    data = await response.json();
-  });
+  export let data;
 </script>
+
+<svelte:head>
+  <title>Pugstagram</title>
+</svelte:head>
 
 <Main>
   <TimeLine posts={data.posts} />
