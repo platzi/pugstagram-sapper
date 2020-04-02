f57bb1b6ec59f3045212875be5da12f65e3802be
Oscar Barajas Tavares <oscar@arepa.dev>
2020-04-02 10:27:42 -0500

    This is where it all begins...

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,69 @@
+const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('sapper/config/webpack.js');
+const pkg = require('./package.json');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const alias = { svelte: path.resolve('node_modules', 'svelte') };
+const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
+const mainFields = ['svelte', 'module', 'browser', 'main'];
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['*', '.mjs', '.js', '.svelte']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+  client: {
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              hydrateble: true,
+              hotReload: false
+            }
+          }
         }
-      },
-      {
-        test: /\.svelte$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'svelte-loader'
-        }
-      }
-    ]
+      ]
+    },
+    mode,
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_EN': JSON.stringify(mode)
+      }),
+    ].filter(Boolean),
+    devtool: dev && 'inline-source-map'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-      filename: './index.html'
-    })
-  ]
-}
+
+  server: {
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkd.despendencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              css: false,
+              generate: 'ssr'
+            }
+          }
+        }
+      ]
+    },
+    mode: process.env.NODE_ENV,
+    performace: {
+      hints: false
+    }
+  },
+};
